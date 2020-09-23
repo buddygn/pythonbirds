de51a16e7175e87ab39355c8f59bc9326ba54f4a
Geison <buddygn@gmail.com>
2020-09-23 01:02:48 -0300

    Criado atributos de instancia nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,12 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
-        return 'Olá'
+        return 'Olá '+self.nome
+
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Geison')
     print(p.cumprimentar())
