f0304e0e2016bf908dcb0871f6bf6c6a21aeaeaa
Geison <buddygn@gmail.com>
2020-09-24 23:56:00 -0300

    Criado atributo complexo

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,12 +1,18 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return 'Olá '+self.nome
 
 
 if __name__ == '__main__':
-    p = Pessoa('Geison')
-    print(p.cumprimentar())
+    geison = Pessoa(nome='Geison')
+    luiz = Pessoa(geison, nome='Luiz')
+    # print(p.cumprimentar())
+
+    print(luiz.cumprimentar())
+    for filho in luiz.filhos:
+        print(filho.nome)
